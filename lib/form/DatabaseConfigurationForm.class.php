8ee57a3824c9872bdf92db3651590aa01e5df104
Darius Krištapavičius <darius@cuf.lt>
2015-06-25 20:54:22 +0300

    installer database form load default settings from config

--- lib/form/DatabaseConfigurationForm.class.php
+++ lib/form/DatabaseConfigurationForm.class.php
@@ -9,42 +9,62 @@
  */
 class DatabaseConfigurationForm extends BaseForm
 {
-  public function configure()
-  {
-    $user = sfContext::getInstance()->getUser();
-    
-    $this->setWidgets(array(
-      'database' => new sfWidgetFormInputText(),
-      'username' => new sfWidgetFormInputText(),
-      'password' => new sfWidgetFormInputText(),
-      'host'     => new sfWidgetFormInputText(),
-      'overwrite'=> new sfWidgetFormInputCheckbox()
-    ));
-       
-    $this->widgetSchema->setLabels(array(
-      'database' => 'Database Name',
-      'username' => 'User Name',
-      'password' => 'Password',
-      'host'     => 'Database Host',
-      'overwrite'=> 'Overwrite previous Siwapp installations'
-    ));
-    
-    $this->setDefaults(array(
-      'database' => $user->getAttribute('database' ,'siwapp'),
-      'username' => $user->getAttribute('username' ,'siwapp'),
-      'host'     => $user->getAttribute('host', 'localhost')
-    ));
-    
-    $this->setValidators(array(
-      'database' => new sfValidatorString(),
-      'username' => new sfValidatorString(),
-      'password' => new sfValidatorString(),
-      'host'     => new sfValidatorString(),
-      'overwrite'=> new sfValidatorPass()
-    ));
-    
-    $this->validatorSchema->setPostValidator(new dbConnectionValidator());
+    public function configure()
+    {
+        $myConnection = \Doctrine_Manager::getInstance()->getConnection('doctrine')->getOptions();
+        $dsnInfo = $this->parseDsn($myConnection['dsn']);
 
-    $this->widgetSchema->setNameFormat('db[%s]');
-  }
+        $this->setWidgets(array(
+            'database' => new sfWidgetFormInputText(),
+            'username' => new sfWidgetFormInputText(),
+            'password' => new sfWidgetFormInputText(),
+            'host' => new sfWidgetFormInputText(),
+            'overwrite' => new sfWidgetFormInputCheckbox()
+        ));
+
+        $this->widgetSchema->setLabels(array(
+            'database' => 'Database Name',
+            'username' => 'User Name',
+            'password' => 'Password',
+            'host' => 'Database Host',
+            'overwrite' => 'Overwrite previous Siwapp installations'
+        ));
+
+        $this->setDefaults(array(
+            'database' => $dsnInfo['db_name'],
+            'username' => $myConnection['username'],
+            'host' => $dsnInfo['host'],
+            'password' => $myConnection['password']
+        ));
+
+        $this->setValidators(array(
+            'database' => new sfValidatorString(),
+            'username' => new sfValidatorString(),
+            'password' => new sfValidatorString(),
+            'host' => new sfValidatorString(),
+            'overwrite' => new sfValidatorPass()
+        ));
+
+        $this->validatorSchema->setPostValidator(new dbConnectionValidator());
+
+        $this->widgetSchema->setNameFormat('db[%s]');
+    }
+
+    /**
+     * Parse dsn info.
+     *
+     * @param string $dsn
+     *
+     * @return array
+     */
+    private function parseDsn($dsn)
+    {
+        $dsnArray = array();
+        preg_match('/dbname=(\w+)/', $dsn, $dbname);
+        $dsnArray['db_name'] = $dbname[1];
+        preg_match('/host=(.*?);/', $dsn, $host);
+        $dsnArray['host'] = $host[1];
+
+        return $dsnArray;
+    }
 }
